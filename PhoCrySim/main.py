71d03792b1008afbb2beb5c60a8667c73408486e
Guilherme Soeiro de Carvalho Caporali <55262460+guilherme13c@users.noreply.github.com>
2022-12-18 20:56:20 -0300

    Update main.py

--- PhoCrySim/main.py
+++ PhoCrySim/main.py
@@ -1,11 +1,16 @@
+# inclui o arquivo de funcionamento do motor de simulacao
 from components import *
 
+# define os inputs
 in1, in2 = 50, 50
+# define o controle
 ctrl = 50
 
+# define o circuito
 T1, D1 = Switch_P.calculate(ctrl, in1)
 T2, D2 = Switch_P.calculate(T1, in2)
 
+# exibe os valores
 print("Controle:\t\t\t", ctrl)
 print("Entrada 1:\t\t\t", in1)
 print("Entrada 2:\t\t\t", in2)
